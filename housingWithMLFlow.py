8f1c540bf03a23ee06743ab31a324c5f2685c96c
vinod <vinod.subramania@tigeranalytics.com>
2022-09-05 11:23:48 +0530

    adding housing mlflow code

---
+++ housingWithMLFlow.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+import os
+import tarfile
+import urllib.request
+import warnings
+import numpy as np
+import pandas as pd
+from scipy.stats import randint
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression,ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import (GridSearchCV, RandomizedSearchCV,
+                                     StratifiedShuffleSplit, train_test_split)
+from sklearn.tree import DecisionTreeRegressor
+import sys
+import mlflow
+import mlflow.sklearn
+
+
+# In[2]:
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+fetch_housing_data()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+housing = load_housing_data()
+
+
+# In[21]:
+
+
+def dataPrep(housing):
+    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+
+    def income_cat_proportions(data):
+        return data["income_cat"].value_counts() / len(data)
+
+
+    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+    compare_props = pd.DataFrame(
+        {
+            "Overall": income_cat_proportions(housing),
+            "Stratified": income_cat_proportions(strat_test_set),
+            "Random": income_cat_proportions(test_set),
+        }
+    ).sort_index()
+    compare_props["Rand. %error"] = (
+        100 * compare_props["Random"] / compare_props["Overall"] - 100
+    )
+    compare_props["Strat. %error"] = (
+        100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+    )
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
+
+    housing = strat_train_set.copy()
+
+    corr_matrix = housing.corr()
+    corr_matrix["median_house_value"].sort_values(ascending=False)
+    housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
+    housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
+    housing["population_per_household"] = housing["population"] / housing["households"]
+
+    housing = strat_train_set.drop(
+        "median_house_value", axis=1
+    )  # drop labels for training set
+    housing_labels = strat_train_set["median_house_value"].copy()
+
+
+    imputer = SimpleImputer(strategy="median")
+
+    housing_num = housing.drop("ocean_proximity", axis=1)
+
+    imputer.fit(housing_num)
+    X = imputer.transform(housing_num)
+
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing.index)
+    housing_tr["rooms_per_household"] = housing_tr["total_rooms"] / housing_tr["households"]
+    housing_tr["bedrooms_per_room"] = (
+        housing_tr["total_bedrooms"] / housing_tr["total_rooms"]
+    )
+    housing_tr["population_per_household"] = (
+        housing_tr["population"] / housing_tr["households"]
+    )
+
+    housing_cat = housing[["ocean_proximity"]]
+    housing_prepared = housing_tr.join(pd.get_dummies(housing_cat, drop_first=True))
+    X_test = strat_test_set.drop("median_house_value", axis=1)
+    y_test = strat_test_set["median_house_value"].copy()
+
+    X_test_num = X_test.drop("ocean_proximity", axis=1)
+    X_test_prepared = imputer.transform(X_test_num)
+    X_test_prepared = pd.DataFrame(
+        X_test_prepared, columns=X_test_num.columns, index=X_test.index
+    )
+    X_test_prepared["rooms_per_household"] = (
+        X_test_prepared["total_rooms"] / X_test_prepared["households"]
+    )
+    X_test_prepared["bedrooms_per_room"] = (
+        X_test_prepared["total_bedrooms"] / X_test_prepared["total_rooms"]
+    )
+    X_test_prepared["population_per_household"] = (
+        X_test_prepared["population"] / X_test_prepared["households"]
+    )
+
+    X_test_cat = X_test[["ocean_proximity"]]
+    X_test_prepared = X_test_prepared.join(pd.get_dummies(X_test_cat, drop_first=True))
+    return housing_prepared,housing_labels,X_test_prepared,y_test
+housing_prepared,housing_labels,X_test_prepared,y_test=dataPrep(housing)
+
+
+# In[36]:
+
+
+def train(housing_prepared,housing_labels):
+    lin_reg = LinearRegression()
+    lin_reg.fit(housing_prepared, housing_labels)
+    housing_predictions = lin_reg.predict(housing_prepared)
+    lin_mse = mean_squared_error(housing_labels, housing_predictions)
+    lin_rmse = np.sqrt(lin_mse)
+    lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+    print(lin_mae)
+train(housing_prepared,housing_labels)
+def eval_metrics(actual, pred):
+    # compute relevant metrics
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+# In[40]:
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.tracking.get_tracking_uri()
+    mlflow.set_experiment("ElasticNet_wine")
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
+    housing_prepared,housing_labels,X_test_prepared,y_test=dataPrep(housing)
+    train_x, train_y,test_x=housing_prepared,housing_labels,X_test_prepared
+    #alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    #l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    alpha,l1_ratio=1,1
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(y_test, predicted_qualities)
+
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        mlflow.sklearn.log_model(lr, "model")
+
+
+# In[ ]:
+
+
+
+
